01032a3cfaf365e7b45330d291ae87c3c8fac5b2
Rundong Liu <53116761+Rundong-Liu@users.noreply.github.com>
2020-12-08 01:55:35 +0000

    Update Ice_Cream_Parlor.py

--- Hackerrank Algo Questions Collection/Search/Ice_Cream_Parlor.py
+++ Hackerrank Algo Questions Collection/Search/Ice_Cream_Parlor.py
@@ -1,1 +1,27 @@
+## Question: Given an unsorted integer list: cost (intger >= 1), an intger: money
+## Return the index of two elements that summed together equal to the integr
+## The question has only unique solution 
+## cost can have differnet positions with the same cost 
 
+def whatFlavors(cost, money):
+    
+    dic = {}
+    for i in range(len(cost)): 
+        if cost[i] < money: 
+            
+            ## Handling repeated caost
+            if cost[i] in dic: 
+                pass
+            ## 
+            
+            else:
+                dic[cost[i]] = i 
+
+        if money - cost[i] in dic and i!= dic[money - cost[i]]: ## Used the constant time operation character of dictionary
+            
+            print (dic[money - cost[i]]+1 , i+1)
+            return 
+          
+## Medium - 35
+## 100% success  
+## O(n)
