6ea3657cfcbaf549b173239ad1b9899f3af6d667
Rundong Liu <53116761+Rundong-Liu@users.noreply.github.com>
2020-12-21 00:32:07 +0000

    Create Greedy Florist.py

---
+++ Hackerrank Algo Questions Collection/Greedy Algorithm/Greedy Florist.py
@@ -0,0 +1,24 @@
+def getMinimumCost(k, c):
+    
+    c = sorted(c, reverse=True)
+    
+    if k >= len(c): 
+        return sum(c)
+
+    else: 
+        
+        result = 0
+        no_purchase = [1]*k
+        
+        for i in range(len(c)): 
+            
+            if i+1 > k: 
+                
+                no_purchase[i%k] += 1 
+                result += no_purchase[i%k] * c[i]
+                
+            else: 
+                
+                result += c[i]
+                
+        return result 
