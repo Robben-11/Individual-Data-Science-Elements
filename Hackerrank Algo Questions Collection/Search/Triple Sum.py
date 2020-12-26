c87d7b49f1cd18daf743d07c52c51ec359ef5ed9
Rundong Liu <53116761+Rundong-Liu@users.noreply.github.com>
2020-12-26 17:26:30 +0000

    Update Triple Sum.py

--- Hackerrank Algo Questions Collection/Search/Triple Sum.py
+++ Hackerrank Algo Questions Collection/Search/Triple Sum.py
@@ -1,3 +1,13 @@
+##IMPORTANT: There is another version of this question using binary search, in which there are some errors, but the idea has also proven under O(nlogn) 
+
+## Step1: Remove the duplicates, we are only talking about combinations of triplets, having repeated numbers in b does not add up. Having repeated elements in a and c 
+## also does not make sense as well since for a given b, having another number repeated in any list does not add a new combination to the existing profile. 
+
+## Step 2: Sort, this allow us to place the counter at the position when each single element in b has gone through the search. So the search does not come back to
+# its original place. 
+
+## Time complexity O(nlogn)
+
 def triplets(a, b, c):
 
     a = sorted(set(a))
